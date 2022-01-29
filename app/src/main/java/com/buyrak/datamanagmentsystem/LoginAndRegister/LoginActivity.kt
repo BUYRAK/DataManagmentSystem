bd96f84b37cdb45dabd0a3ebefa947da09def873
buyrak <siyahmerkez@gmail.com>
2022-01-29 06:35:07 +0300

    update 29/01

--- app/src/main/java/com/buyrak/datamanagmentsystem/LoginAndRegister/LoginActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/LoginAndRegister/LoginActivity.kt
@@ -1,12 +1,19 @@
 package com.buyrak.datamanagmentsystem.LoginAndRegister
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.buyrak.datamanagmentsystem.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        txtRegisterPage.setOnClickListener {
+            var intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/buyrak/datamanagmentsystem/LoginAndRegister/SplashActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/LoginAndRegister/SplashActivity.kt
@@ -1,12 +1,45 @@
 package com.buyrak.datamanagmentsystem.LoginAndRegister
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
 import com.buyrak.datamanagmentsystem.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onResume() {
+
+        object : CountDownTimer(3000,1000){
+
+            override fun onFinish() {
+                /*
+                if (FirebaseAuth.getInstance().currentUser != null){
+
+                    var intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }else{
+                    var intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+                 */
+                var intent = Intent(this@SplashActivity, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+        }.start()
+        super.onResume()
     }
 }
