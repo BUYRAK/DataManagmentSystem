5ab1265c66a5dbeace081d059fe240722b4147b6
buyrak <siyahmerkez@gmail.com>
2022-02-07 15:56:58 +0300

    Update 07/02/22

--- app/src/main/java/com/buyrak/datamanagmentsystem/SettingsOperations/SettingsActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/SettingsOperations/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package com.buyrak.datamanagmentsystem.SettingsOperations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.buyrak.datamanagmentsystem.MainOperations.MainActivity
 import com.buyrak.datamanagmentsystem.R
 import com.buyrak.datamanagmentsystem.utils.BottomNavigationHelper
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,5 +14,11 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         BottomNavigationHelper.setUpNavigationView(this, bottomNavigationView,  bottomNavigationView.menu, ACTIVITY_NO)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        this.startActivity(intent)
     }
 }
--- app/src/main/java/com/buyrak/datamanagmentsystem/TasksOperations/TasksActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/TasksOperations/TasksActivity.kt
@@ -1,10 +1,12 @@
 package com.buyrak.datamanagmentsystem.TasksOperations
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.buyrak.datamanagmentsystem.MainOperations.MainActivity
 import com.buyrak.datamanagmentsystem.R
 import com.buyrak.datamanagmentsystem.utils.BottomNavigationHelper
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 
 class TasksActivity : AppCompatActivity() {
     val ACTIVITY_NO = 1
@@ -12,5 +14,11 @@ class TasksActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
         BottomNavigationHelper.setUpNavigationView(this, bottomNavigationView,  bottomNavigationView.menu, ACTIVITY_NO)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        this.startActivity(intent)
     }
 }
