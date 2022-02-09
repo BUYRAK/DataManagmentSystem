5692131ed12bb5d01e97722c4cb165beed82ba69
buyrak <siyahmerkez@gmail.com>
2022-02-10 02:27:24 +0300

    Initial commit

--- app/src/main/java/com/buyrak/datamanagmentsystem/Profile/ProfileActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/Profile/ProfileActivity.kt
@@ -1,14 +1,28 @@
 package com.buyrak.datamanagmentsystem.Profile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.buyrak.datamanagmentsystem.MainOperations.MainActivity
 import com.buyrak.datamanagmentsystem.R
+import com.buyrak.datamanagmentsystem.utils.BottomNavigationHelper
 import com.buyrak.datamanagmentsystem.utils.Functions
-import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
+    val ACTIVITY_NO = 2
     override fun onCreate(savedInstanceState: Bundle?) {
+        val user = Functions.currentUser
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        BottomNavigationHelper.setUpNavigationView(this, bottomNavigationView,  bottomNavigationView.menu, ACTIVITY_NO)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        this.startActivity(intent)
     }
 }
--- app/src/main/java/com/buyrak/datamanagmentsystem/SettingsOperations/SettingsActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/SettingsOperations/SettingsActivity.kt
@@ -1,24 +1,12 @@
 package com.buyrak.datamanagmentsystem.SettingsOperations
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.buyrak.datamanagmentsystem.MainOperations.MainActivity
 import com.buyrak.datamanagmentsystem.R
-import com.buyrak.datamanagmentsystem.utils.BottomNavigationHelper
-import kotlinx.android.synthetic.main.activity_main.*
 
 class SettingsActivity : AppCompatActivity() {
-    val ACTIVITY_NO = 2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-        BottomNavigationHelper.setUpNavigationView(this, bottomNavigationView,  bottomNavigationView.menu, ACTIVITY_NO)
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        val intent = Intent(this, MainActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
-        this.startActivity(intent)
-    }
+        setContentView(R.layout.activity_profile)
+        }
 }
