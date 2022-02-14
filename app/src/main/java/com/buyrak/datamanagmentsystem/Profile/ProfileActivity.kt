c4908a4954016f3c941985df6e084a117f6b60c5
buyrak <siyahmerkez@gmail.com>
2022-02-15 02:09:24 +0300

    Initial commit

--- app/src/main/java/com/buyrak/datamanagmentsystem/Profile/ProfileActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/Profile/ProfileActivity.kt
@@ -1,36 +1,26 @@
 package com.buyrak.datamanagmentsystem.Profile
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.buyrak.datamanagmentsystem.MainOperations.MainActivity
 import com.buyrak.datamanagmentsystem.R
 import com.buyrak.datamanagmentsystem.utils.BottomNavigationHelper
-import com.buyrak.datamanagmentsystem.utils.Functions
-import com.buyrak.datamanagmentsystem.utils.User
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.ktx.Firebase
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 import kotlinx.android.synthetic.main.activity_profile.*
 
+
 class ProfileActivity : AppCompatActivity() {
     val ACTIVITY_NO = 2
     var currentUser = FirebaseAuth.getInstance().currentUser
-    var referanceUser = FirebaseDatabase.getInstance().reference.child("users")
+    var currentUserDb = FirebaseDatabase.getInstance().reference.child("users")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         BottomNavigationHelper.setUpNavigationView(this, bottomNavigationView,  bottomNavigationView.menu, ACTIVITY_NO)
-        //readUserDataProfile()
-
-       // txtProfileDisplayName.text =
-        // referanceUser.child(currentUser!!.uid).orderByChild("email").limitToFirst(1).toString()
 
     }
 
@@ -41,25 +31,5 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     fun readUserDataProfile(){
-        var currentUser = FirebaseAuth.getInstance().currentUser
-        var referance = FirebaseDatabase.getInstance().reference
-
-        var queryEqualUser = referance.child("users")
-            .orderByKey()
-            .equalTo(currentUser!!.uid)
-
-        queryEqualUser.addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot){
-                for(singleSnapShot in snapshot.children){
-                    val getUser = singleSnapShot.getValue(User::class.java)
-
-                }
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
-            }
-
-        })
     }
 }
