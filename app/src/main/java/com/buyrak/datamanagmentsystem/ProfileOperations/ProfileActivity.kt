b8a5fc57829d63515f913855a6e92efce05658bf
buyrak <siyahmerkez@gmail.com>
2022-03-08 03:37:39 +0300

    Initial commit

--- app/src/main/java/com/buyrak/datamanagmentsystem/ProfileOperations/ProfileActivity.kt
+++ app/src/main/java/com/buyrak/datamanagmentsystem/ProfileOperations/ProfileActivity.kt
@@ -2,13 +2,16 @@ package com.buyrak.datamanagmentsystem.ProfileOperations
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.buyrak.datamanagmentsystem.MainOperations.MainActivity
 import com.buyrak.datamanagmentsystem.R
 import com.buyrak.datamanagmentsystem.utils.BottomNavigationHelper
+import com.buyrak.datamanagmentsystem.utils.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
+import kotlinx.android.synthetic.main.activity_profile.*
 
 
 class ProfileActivity : AppCompatActivity() {
@@ -17,8 +20,32 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         BottomNavigationHelper.setUpNavigationView(this, bottomNavigationView,  bottomNavigationView.menu, ACTIVITY_NO)
-
+        readUserData()
       }
+
+    private fun readUserData(){
+        var referance = FirebaseDatabase.getInstance().reference
+        var user = FirebaseAuth.getInstance().currentUser
+
+        var query = referance.child("users")
+            .orderByKey()
+            .equalTo(user!!.uid)
+        query.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                for (singleSnapshot in snapshot.children){
+                    var readUser = singleSnapshot.getValue(User::class.java)
+                   Log.e("","${readUser!!.fullName}")
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
 
     override fun onBackPressed() {
         super.onBackPressed()
